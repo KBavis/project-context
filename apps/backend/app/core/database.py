036c82fefbd42471065bb5300c92cd3a0f1a2097
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-20 18:54:54 -0400

    feat: db session factory for FastAPI runtime dependency injection

--- apps/backend/app/core/database.py
+++ apps/backend/app/core/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from .config import settings
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session, sessionmaker
+from typing import Generator
 
 
 
@@ -17,6 +18,32 @@ def _make_engine():
 
 engine: Engine = _make_engine()
 
+
+# create session factory for FastAPI runtime 
+SessionLocal: sessionmaker[Session] = sessionmaker(
+    autoflush=False,
+    autocommit=False
+)
+
+def get_db_session() -> Generator[Session]:
+    """
+    Create transactional DB session that will commit 
+    in the case that no exceptions occurred, or else 
+    it will rollback 
+
+    Note: This function creates a Generator 
+    that works will with FastAPI's "Depends" functionality
+    """
+    db = SessionLocal
+
+    try:
+        yield db 
+        db.commit() 
+    except Exception:
+        db.rollback() 
+        raise 
+    finally:
+        db.close() 
 
 
 
