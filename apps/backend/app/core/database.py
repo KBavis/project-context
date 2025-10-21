f951d1155028a22e1eb2cec82976a23ee599c678
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-21 00:31:14 -0400

    feat: finish inital impl for creating transactional sync db sessions

--- apps/backend/app/core/database.py
+++ apps/backend/app/core/database.py
@@ -3,11 +3,12 @@ from .config import settings
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session, sessionmaker
 from typing import Generator
+from ..models import (Base,DataSource, IngestionJob)
 
 
 
 # create engine 
-def _make_engine():
+def _make_engine() -> Engine:
     """Create database Engine"""
     engine = create_engine(
         settings.DB_URL,
@@ -22,10 +23,11 @@ engine: Engine = _make_engine()
 # create session factory for FastAPI runtime 
 SessionLocal: sessionmaker[Session] = sessionmaker(
     autoflush=False,
-    autocommit=False
+    autocommit=False,
+    bind=engine
 )
 
-def get_db_session() -> Generator[Session]:
+def get_db_session() -> Generator[Session, None, None]:
     """
     Create transactional DB session that will commit 
     in the case that no exceptions occurred, or else 
@@ -34,7 +36,7 @@ def get_db_session() -> Generator[Session]:
     Note: This function creates a Generator 
     that works will with FastAPI's "Depends" functionality
     """
-    db = SessionLocal
+    db = SessionLocal()
 
     try:
         yield db 
@@ -46,4 +48,9 @@ def get_db_session() -> Generator[Session]:
         db.close() 
 
 
+def init_db() -> None:
+    """
+    Initalize necessary DB tables used through application 
+    """
+    Base.metadata.create_all(bind=engine)
 
