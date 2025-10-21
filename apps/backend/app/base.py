b915c322dfa84f3194757d9a415c8a3e21bd018b
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-21 00:29:18 -0400

    feat: add contextlifecycle manager for initalizing db tables on startup if needed

--- apps/backend/app/base.py
+++ apps/backend/app/base.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
-from .core import settings
+from .core import (settings, init_db, engine)
+from typing import Generator
+from contextlib import asynccontextmanager
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Async context manager for initializing necessary models and then 
+    disposing of DB engine once app's shutdown
+    """
+    init_db() 
+    yield
+    engine.dispose()
 
 
 def create_app() -> FastAPI:
@@ -11,7 +22,8 @@ def create_app() -> FastAPI:
     # TODO: Setup logging 
 
     app = FastAPI(
-        title=settings.PROJECT_NAME
+        title=settings.PROJECT_NAME,
+        lifespan=lifespan
     )
 
     #TODO: Add Middleware For CORS & Exception Handlers 
