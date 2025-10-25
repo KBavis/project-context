61b0fc08fa0a2deade800a41f5dd4a226e669485
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-25 15:52:37 -0400

    feat: future proof for ability to link epics to project

--- apps/backend/app/pydantic/project..py
+++ apps/backend/app/pydantic/project..py
@@ -6,3 +6,4 @@ from sqlalchemy.dialects.postgresql import UUID
 class Project(BaseModel):
     name: str
     teams: Optional[List[UUID]] = [] # Note: once Team model is setup, this should likely be enforced
+    epics: Optional[List[str]] = [] # List of Jira Epic's, that we can later used to determine if relevant "commits" should be include in collection
