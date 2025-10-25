c2317d116f1133520ef1c4539cc24629b208dc60
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-25 15:52:53 -0400

    feat: add ability to persist list of epics corresponding to Project

--- apps/backend/app/models/project.py
+++ apps/backend/app/models/project.py
@@ -1,6 +1,8 @@
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import TYPE_CHECKING, List
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy import String
 
 if TYPE_CHECKING:
     from .project_data import ProjectData
@@ -10,6 +12,9 @@ class Project(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     project_name: Mapped[str] = mapped_column(nullable=False)
+    epics: Mapped[List[str]] = mapped_column(ARRAY(String))
+
+    #TODO: Create association table for Team and Project
 
     # many to many relationship with DataSource
     project_data: Mapped[List["ProjectData"]] = relationship(back_populates="project")
