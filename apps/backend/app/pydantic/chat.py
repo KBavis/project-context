b0a8f114e0509a311c9c31567ea964383150a3bb
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-25 20:22:22 -0400

    refactor: rename to request

--- apps/backend/app/pydantic/chat.py
+++ apps/backend/app/pydantic/chat.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-class Chat(BaseModel):
+class ChatRequest(BaseModel):
     content: str 
     project_id: int 
     
--- apps/backend/app/pydantic/project.py
+++ apps/backend/app/pydantic/project.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 from typing import List, Optional
-from sqlalchemy.dialects.postgresql import UUID
+from uuid import UUID
 
 
-class Project(BaseModel):
+class ProjectRequest(BaseModel):
     name: str
     teams: Optional[List[UUID]] = [] # Note: once Team model is setup, this should likely be enforced
     epics: Optional[List[str]] = [] # List of Jira Epic's, that we can later used to determine if relevant "commits" should be include in collection
