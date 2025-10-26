9633259877f8b9830c3be974305d8307e19103c2
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-25 20:27:40 -0400

    feat: add ProjectData association

--- apps/backend/app/models/__init__.py
+++ apps/backend/app/models/__init__.py
@@ -2,11 +2,13 @@ from .base import Base
 from .data_source import DataSource
 from .ingestion_job import IngestionJob
 from .project import Project
+from .project_data import ProjectData
 
 
 __all__ = [
     'Base',
     'DataSource',
     'IngestionJob',
-    'Project'
+    'Project', 
+    'ProjectData'
 ]
