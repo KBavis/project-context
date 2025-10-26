18b26e7d06908025e46be7fa15a1e0801feae1b9
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-25 20:57:50 -0400

    feat: establihs association between project and data sources and return dict instead of db objects

--- apps/backend/app/services/data_source.py
+++ apps/backend/app/services/data_source.py
@@ -1,8 +1,9 @@
 from sqlalchemy.orm import Session
 from app.pydantic import DataSourceRequest
-from app.models import DataSource, Project
+from app.models import DataSource, Project, ProjectData
 from uuid import UUID
 from sqlalchemy import select
+from typing import List
 
 
 class DataSourceService:
@@ -10,7 +11,7 @@ class DataSourceService:
         self.db = db 
     
 
-    def create_data_source(self, request: DataSourceRequest) -> DataSource:
+    def create_data_source(self, request: DataSourceRequest) -> dict:
         """
         Functionality to persist new DataSource based on specified request
         """
@@ -30,26 +31,44 @@ class DataSourceService:
 
         # retrieve Projects corresponding to IDs specified in request
         project_ids = request.project_ids
-        stmt = select(Project).where(Project.id in project_ids)
+        stmt = select(Project).where(Project.id.in_(project_ids))
         projects = self.db.execute(stmt).scalars().all()
 
         # ensure each project retrieved successfully 
         if len(projects) != len(project_ids):
-            raise Exception(f"Failed to retrieve all Projects specified by the Project IDs {project_ids}")
+            found_ids = {str(project.id) for project in projects}
+            missing_ids = set(request.project_ids) - found_ids
+            raise Exception(f"Failed to retrieve all Projects corresponding to follwoing Project Ids: {missing_ids}")
 
-        # TODO: Create assocation        
+        # create associations 
+        for project in projects:     
+            assocation = ProjectData(
+                project_id=project.id, 
+                data_source_id=data_source.id
+            )
+            data_source.project_data.append(assocation)
 
-        return data_source
+        return {
+            "id": data_source.id, 
+            "provider": data_source.provider, 
+            "linked_projects": project_ids
+        }
     
 
 
-    def get_project_data_sources(self, project_id: UUID):
+    def get_project_data_sources(self, project_id: UUID) -> List[dict]:
         """
         Functionality to retreive persisted data_sourcs that correspond to particular Project ID
         """
 
         stmt = select(DataSource).join(DataSource.project_data).where(Project.id == project_id)
-        return self.db.execute(stmt).scalars().all()
+        data_sources = self.db.execute(stmt).scalars().all()
+
+        return [{
+            "id": data_source.id, 
+            "provider" : data_source.provider,
+            "source_type" : data_source.source_type
+        } for data_source in data_sources]
 
 
 
