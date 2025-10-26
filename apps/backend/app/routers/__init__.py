6881921c4b674d03faa72a4035e399ffbdfbac1e
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-25 20:56:49 -0400

    feat: include projects router in main app router

--- apps/backend/app/routers/__init__.py
+++ apps/backend/app/routers/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .data_source import router as data_source_router 
 from .ingestion_job import router as ingestion_joh_router 
+from .project import router as project_router
 
 app_router = APIRouter(prefix="/api")
 app_router.include_router(data_source_router)
 app_router.include_router(ingestion_joh_router)
+app_router.include_router(project_router)
 
 __all__ = ['app_router']
