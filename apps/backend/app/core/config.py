3cbec3033dea12aa1cb8515ad9695013df65d9ba
Kellen Bavis <kellenrbavis@gmail.com>
2025-10-28 23:44:33 -0400

    feat: add settings for configuring both DOCS and CODE provider, as we want to provide option to have seperate embedding models since some are better at one more than the other

--- apps/backend/app/core/config.py
+++ apps/backend/app/core/config.py
@@ -9,6 +9,14 @@ class Settings(BaseSettings):
     VECTOR_DB_HOST: str = "localhost"
     VECTOR_DB_PORT: int = 8000
 
+    DOCS_EMBEDDING_PROVIDER: str = "HuggingFace"
+    DOCS_EMBEDDING_API_KEY: str = "api-key"
+    DOCS_EMBEDDING_MODEL: str = "BAAI/bge-large-en-v1.5"
+
+    CODE_EMBEDDING_PROVIDER: str = "HuggingFace"
+    CODE_EMBEDDING_API_KEY: str = "api-key"
+    CODE_EMBEDDING_MODEL: str = "jinaai/ina-embeddings-v2-base-code"
+
     model_config = SettingsConfigDict(
         env_file=Path(__file__).resolve().parents[2] / '.env',
         env_file_encoding="utf-8"
