d2ffd297d7fe26501fd4ed29a68502cc53712c21
Kellen Bavis <kellenrbavis@gmail.com>
2025-11-07 21:13:54 -0500

    feat: add IngestionJobService to list of available service

--- apps/backend/app/services/__init__.py
+++ apps/backend/app/services/__init__.py
@@ -1,7 +1,9 @@
 from .data_source import DataSourceService
 from .project import ProjectService
+from .ingestion_job import IngestionJobService
 
 __all__ = [
     'DataSourceService', 
-    'ProjectService'
+    'ProjectService',
+    'IngestionJobService'
 ]
