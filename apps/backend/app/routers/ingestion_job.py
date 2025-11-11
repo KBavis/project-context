b476684a3fe793716a3305dd8a1933b81d64f49e
Kellen Bavis <kellenrbavis@gmail.com>
2025-11-10 23:12:01 -0500

    feat: add  ability to create ingestion job for specific project

--- apps/backend/app/routers/ingestion_job.py
+++ apps/backend/app/routers/ingestion_job.py
@@ -22,8 +22,22 @@ def create_ingestion_job(data_source_id: UUID, db: Session = Depends(get_db_sess
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"{str(e)}"
         )
-        
 
+
+@router.post("/{data_source_id}/{project_id}", summary="Kick off ingestion of data from a datasource for a specific Project")
+def create_ingestion_job(data_source_id: UUID, project_id: UUID, db: Session = Depends(get_db_session)):
+    """
+    Kick off ingestion job for a datasource for only data corresponding to specified Project
+    """
+
+    try:
+        svc = IngestionJobService(db)
+        return svc.run_ingestion_job(data_source_id, project_id)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{str(e)}"
+        )
 
 
 @router.get("/", summary="Retrieve all ingestion jobs")
