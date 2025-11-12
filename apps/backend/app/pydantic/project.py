6df07d6d51b6229f1b1f1bb208c9ef06983bb341
Kellen Bavis <kellenrbavis@gmail.com>
2025-11-11 23:31:59 -0500

    feat: add model configs to project creation request

--- apps/backend/app/pydantic/project.py
+++ apps/backend/app/pydantic/project.py
@@ -10,9 +10,9 @@ class ProjectRequest(BaseModel):
 
     # allow for model configuration during project creation
     code_embedding_provider: Optional[str] = settings.CODE_EMBEDDING_PROVIDER
-    docs_embedding_model: Optional[str] = settings.DOCS_EMBEDDING_MODEL
-    code_embedding_provider: Optional[str] = settings.DOCS_EMBEDDING_PROVIDER
-    code_embedding_model: Optional[str] = settings.CODE_EMBEDDING_MODEL
+    code_embedding_model: Optional[str] = settings.DOCS_EMBEDDING_MODEL
+    docs_embedding_provider: Optional[str] = settings.DOCS_EMBEDDING_PROVIDER
+    docs_embedding_model: Optional[str] = settings.CODE_EMBEDDING_MODEL
 
     teams: Optional[List[UUID]] = [] # Note: once Team model is setup, this should likely be enforced
     epics: Optional[List[str]] = [] # List of Jira Epic's, that we can later used to determine if relevant "commits" should be include in collection
