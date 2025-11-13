d0088bd2d9bce9d3666fcf02f73b90a4abe1e63e
Kellen Bavis <kellenrbavis@gmail.com>
2025-11-12 23:03:41 -0500

    feat: configure embedding provider to also retrieve tokenizer for dockling chunking

--- apps/backend/app/embeddings/manager.py
+++ apps/backend/app/embeddings/manager.py
@@ -1,18 +1,20 @@
 from app.core import settings
+from app.models import ModelConfigs
+from transformers import AutoTokenizer
 import logging
 
 
 class EmbeddingManager:
 
-    def __init__(self, code_provider, code_model, docs_provider, docs_model):
+    def __init__(self, model_configs: ModelConfigs):
 
         # Coding Embedding Specific Values
-        self._code_provider = code_provider
-        self._code_model = code_model
+        self._code_provider = model_configs.code_embedding_provider
+        self._code_model = model_configs.code_embedding_model 
 
         # Docs Embedding Specific Values
-        self._docs_provider = docs_provider
-        self._docs_model = docs_model
+        self._docs_provider = model_configs.docs_embedding_provider
+        self._docs_model = model_configs.docs_embedding_model
 
     def get_embedding_model(self, source_type: str):
         """
@@ -24,6 +26,61 @@ class EmbeddingManager:
             if source_type == "DOCS"
             else self.get_code_embedding_model()
         )
+    
+    def get_tokenizer(self, source_type):
+        """
+        Retrieve configured tokenizer based on configured models and provider 
+        """
+
+        return (
+            self.get_docs_tokenizer()
+            if source_type == "DOCS"
+            else self.get_code_tokenizer()
+        )
+    
+    def get_code_tokenizer(self):
+        """
+        Use Docling to retrieve code tokenizer corresponding to 
+        configured model and provider 
+        """
+
+        match self._code_provider:
+
+            case "HuggingFace":
+                from docling_core.transforms.chunker.tokenizer.huggingface import HuggingFaceTokenizer
+                return HuggingFaceTokenizer(
+                    tokenizer=AutoTokenizer.from_pretrained(self._code_model)
+                )
+            case _:
+                logging.error(
+                    f"The embedidng provider specified, '{self._code_provider}', is not curretly set up for this application"
+                )
+                raise Exception(
+                    f"Invalid embedding provider specified: {self._code_provider}"
+                )
+
+
+    def get_docs_tokenizer(self):
+        """
+        Use Docling to retrieve docs tokenizer corresponding to 
+        configured model and provider 
+        """
+        
+        match self._docs_provider:
+
+            case "HuggingFace":
+                from docling_core.transforms.chunker.tokenizer.huggingface import HuggingFaceTokenizer
+                return HuggingFaceTokenizer(
+                    tokenizer=AutoTokenizer.from_pretrained(self._docs_model)
+                )
+            case _:
+                logging.error(
+                    f"The embedidng provider specified, '{self._docs_provider}', is not curretly set up for this application"
+                )
+                raise Exception(
+                    f"Invalid embedding provider specified: {self._docs_provider}"
+                )
+
 
     def get_docs_embedding_model(self):
 
@@ -36,7 +93,7 @@ class EmbeddingManager:
                 return HuggingFaceEmbedding(model_name=settings.DOCS_EMBEDDING_MODEL)
             case _:
                 logging.error(
-                    f"The embedidng provider specified, '{self._code_provider}', is not curretly set up for this application"
+                    f"The embedidng provider specified, '{self._docs_provider}', is not curretly set up for this application"
                 )
                 raise Exception(
                     f"Invalid embedding provider specified: {self._docs_provider}"
