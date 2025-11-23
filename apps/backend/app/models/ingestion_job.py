1019d4cede36a89e2b3bc2027515f2894ddfc351
Kellen Bavis <kellenrbavis@gmail.com>
2025-11-22 22:57:57 -0500

    feat: use enum for processing status

--- apps/backend/app/models/ingestion_job.py
+++ apps/backend/app/models/ingestion_job.py
@@ -1,8 +1,9 @@
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, text
+from sqlalchemy import ForeignKey, text, Enum as SQLEnum
 from uuid import UUID
 from typing import TYPE_CHECKING
+from enum import Enum
 
 
 # avoid warning
@@ -10,13 +11,18 @@ if TYPE_CHECKING:
     from .data_source import DataSource
 
 
+class ProcessingStatus(Enum):
+    SUCCESS = "success"
+    IN_PROGRESS = "in_progress"
+    FAILED = "failed"
+
 class IngestionJob(Base):
     __tablename__ = "ingestion_job"
 
     id: Mapped[UUID] = mapped_column(
         primary_key=True, index=True, server_default=text("gen_random_uuid()")
     )
-    processing_status: Mapped[str] = mapped_column(nullable=False)
+    processing_status: Mapped[ProcessingStatus] = mapped_column(SQLEnum(ProcessingStatus), nullable=False)
     data_source_id: Mapped[UUID] = mapped_column(ForeignKey("data_source.id"))
 
     data_source: Mapped["DataSource"] = relationship(back_populates="ingestion_jobs")
