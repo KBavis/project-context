23d59d88048212b451901e16f13c5c657c6a6362
Kellen Bavis <kellenrbavis@gmail.com>
2025-11-24 23:10:01 -0500

    feat: query for files

--- apps/backend/app/services/file.py
+++ apps/backend/app/services/file.py
@@ -1,16 +1,48 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 from uuid import UUID
 
-from app.pydantic import File
-
-
+from app.models import File
 
 class FileService:
 
     def __init__(self, db: Session):
         self.db = db
     
+    
+    def get_file_by_hash(self, hash: str) -> File:
+        """
+        Find File by its hashed content 
+
+        Args:
+            hash (str): the hash to search for 
+        """
+        stmt = (
+            select(File)
+            .where(File.hash == hash)
+        )
+
+        return self.db.execute(stmt).scalars().one_or_none()
+
+    
+    def get_file_by_path_and_data_source(self, path: str, data_source_id: UUID) -> File: 
+        """
+        Get File by path and its data source ID
+
+        Args:
+            path (str): relevant path of file 
+            data_source_id (UUID): ID of the data source this file belongs to 
+        """
+
+        stmt = (
+            select(File)
+            .where(File.path == path, File.data_source_id == data_source_id)
+        )
+
+        return self.db.execute(stmt).scalars().one_or_none()
+
+        
 
     def update_existing_file():
         """
