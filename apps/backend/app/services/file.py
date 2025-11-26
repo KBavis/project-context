9d7de1a505aaef20263a4d1ef03eab0e099e89b7
Kellen Bavis <kellenrbavis@gmail.com>
2025-11-26 00:41:16 -0500

    feat: add logic for inserting or updating File record in database

--- apps/backend/app/services/file.py
+++ apps/backend/app/services/file.py
@@ -1,9 +1,19 @@
 from sqlalchemy.orm import Session
-from sqlalchemy import select
+from sqlalchemy import select, or_, and_
 
 from uuid import UUID
+import logging
 
 from app.models import File
+from app.pydantic import File as FilePydantic
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.models import DataSource
+
+
+logger = logging.getLogger(__name__)
 
 class FileService:
 
@@ -44,32 +54,68 @@ class FileService:
 
         
 
-    def update_existing_file():
+    def update_existing_file(self, file: FilePydantic, data_source: DataSource):
         """
         Functionality to update an existing File 
+
+        Args:
+            file (FilePydantic): file with relevant updates 
         """
+
+        # attempt to find file by either path OR hash, and the respective DataSource ID
+        files = self.db.query(File).filter(
+            and_(
+                or_(
+                    File.hash == file.hash,
+                    File.path == file.path
+                ),
+                File.data_source_id == data_source.id
+            )
+        ).all() 
+
+        if not files:
+            logger.debug(f"No files found corresponding to file_hash={file.hash} or file_path={file.path} in DB")
+            return 
+
+
+        if len(files) > 1:
+            logger.error(f"Two seperate files found corresponding to file_hash={file.hash} and file_path={file.path} in DB for dataSource={data_source.id}")
+            raise Exception(f"Multiple files found by corresponding to hash={file.hash} and/or path={file.path}")
+        
+        existing_file = files[0]
+        existing_file.hash = file.hash
+        existing_file.name = file.file_name
+        existing_file.size = file.size 
+        existing_file.path = file.path
+        existing_file.file_extension = file.file_type
+        
+        self.db.flush()
+            
+        
     
 
-    def add_new_file():
+    def add_new_file(self, file: FilePydantic, data_source: DataSource):
         """
         Functionality to insert a new File & corresponding FileCollection record
         """
+
+        new_file = File(
+            hash=file.hash,
+            size=file.size,
+            file_extension=file.file_type,
+            name=file.file_name,
+            path=file.path,
+            data_source_id=data_source.id,
+        )
+
+
+        self.db.add(new_file)
+        self.db.flush()
     
 
-    def check_if_file_exists(file_id: str, project_id: UUID):
+    def add_file_to_collections(self, file: FilePydantic, data_source: DataSource):
         """
-        Check if file exists in relevant collection 
-        """
+        TODO: Complete  me 
 
-
-    def generate_file_hash(file: File):
+        Add a newly created file to project collections
         """
-        Generate file hash based on file content
-        """
-    
-
-    def check_if_file_changed(file: File):
-        """
-        Check if the file content has changed since last being ingested 
-        """
-    
