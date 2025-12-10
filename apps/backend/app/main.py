a82286c1086398e99bd354291c70518c62726dda
Kellen Bavis <kellenrbavis@gmail.com>
2025-12-10 00:35:30 -0500

    chore: fix issue with reloads blocking subsequent http requests when files downloading

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -4,4 +4,15 @@ from .base import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "app.main:app", 
+        host="0.0.0.0", 
+        port=8000, 
+        reload=True,
+        reload_excludes=[
+            "tmp/*",
+            "tmp/**/*",  
+            "*.pyc",
+            "__pycache__/*"
+        ]
+    )
