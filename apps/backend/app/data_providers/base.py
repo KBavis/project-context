e5c38810e5a41d3e90593adfe12e3f6ae2c211b1
Kellen Bavis <kellenrbavis@gmail.com>
2025-12-11 16:52:48 -0500

    refactor: remove usage of worker thread when retreiving data
    given data retrieval is just heavy I/O (http requests & db interactions), simply using async connections & requests is sufficient to not block other threads

--- apps/backend/app/data_providers/base.py
+++ apps/backend/app/data_providers/base.py
@@ -23,24 +23,31 @@ class DataProvider(ABC):
         self.file_service = FileService(db_session=db_session)
     
 
-
     @classmethod
-    async def run_ingestion(cls: Type, data_source: DataSource, job_pk: UUID):
-        """
-        Re-usable, asbtract method for offloading the retrival of data from the specified provider 
+    async def run_ingestion(provider_class: Type, data_source: DataSource, job_pk: UUID):
 
-        Args:
-            cls (Type): the specific DataProvider subclass 
-            data_source (DataSource): the data source this ingestion job is being ran for 
-            job_pk (UUID): the unique ID for the ingestion job 
-        """
+        # create async DB session for data retrieval 
+        session_maker = get_async_session_maker()
+        
+        async with session_maker() as session:
+            try:
 
-        await asyncio.to_thread(
-            _sync_ingestion,
-            cls,
-            data_source,
-            job_pk
-        )
+                # instantiate concrete provider
+                provider_instance = provider_class(
+                    data_source=data_source, 
+                    url=data_source.url, 
+                    job_pk=job_pk,
+                    db_session=session
+                )
+
+                logger.info(f"Ingesting data from DataProvider={provider_class} for IngestionJob={job_pk}")
+                await provider_instance.ingest_data() 
+
+                await session.commit() 
+            except Exception as e:
+                logger.error(f"Failure occurred while ingesting data from DataSource = {str(e)}")
+                await session.rollback() 
+                raise 
 
 
     @abstractmethod
@@ -58,49 +65,3 @@ class DataProvider(ABC):
     @abstractmethod
     def _get_request_headers(self):
         pass
-
-
-def _sync_ingestion(provider_class: Type, data_source: DataSource, job_pk: UUID):
-    """
-    Synchronous wrapper function for executing 'ingest_data'
-
-    Args:
-        provider_class (Type): the specific DataProvider subclass 
-        data_source (DataSource): the data source this ingestion job is being ran for 
-        job_pk (UUID): the unique ID for the ingestion job 
-    """
-
-    async def run():
-        
-        # create async DB session (NOTE: This will create new Async DB Engine on this worker thread)
-        session_maker = get_async_session_maker()
-        
-        async with session_maker() as session:
-            try:
-
-                # instantiate concrete provider in worker thread  
-                provider_instance = provider_class(
-                    data_source=data_source, 
-                    url=data_source.url, 
-                    job_pk=job_pk,
-                    db_session=session
-                )
-
-                logger.info(f"Ingesting data from DataProvider={provider_class} via WorkerThread={threading.get_ident()}")
-                await provider_instance.ingest_data() 
-
-                await session.commit() 
-            except Exception as e:
-                logger.error(f"Failure occurred while ingesting data from DataSource = {str(e)}")
-                await session.rollback() 
-                raise 
-        
-
-    try:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-        loop.run_until_complete(run())
-    finally:
-        loop.close() 
-        asyncio.set_event_loop(None)
