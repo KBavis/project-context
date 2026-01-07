1f3981a73ba21db064b528e2edaae6529bcc6707
Kellen Bavis <kellenrbavis@gmail.com>
2026-01-06 21:35:06 -0500

    chore: add todo

--- apps/backend/app/api/routers/query.py
+++ apps/backend/app/api/routers/query.py
@@ -15,6 +15,7 @@ async def query(
     Perform a one-time query against the ingested documentation and code for a specified Project.
     """
     try:
+        # TODO: Run this as a background task in order to give user fast response and avoid timeouts
         response = await svc.execute_simple_query(request.query, request.project_id)
         return response
     except Exception as e:
