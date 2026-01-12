e44910ebf083fe55827990702d33f0d68a741964
Kellen Bavis <kellenrbavis@gmail.com>
2026-01-11 23:13:15 -0500

    feat: continue with impl for calculating max context length using host machine limiation and model specific configs

--- apps/backend/app/llm/providers/ollama.py
+++ apps/backend/app/llm/providers/ollama.py
@@ -21,8 +21,78 @@ class OllamaLLM(LLMBase):
         Return the maximum context length for the Ollama LLM.
         """
 
+        total_vram = self._get_total_vram()
 
-        # get total VRAM for current machine
+        model_stats = self._get_model_stats()
+        logger.debug(f"{self.model_name} Statistics: {model_stats}")
+
+        return self._calculate_max_context_length()
+    
+    def _calculate_max_context_length(self, model_stats: dict, total_vram: float) -> int: 
+        """
+        Calculate the max context length for the given model utilizing model 
+        specific stats and the host machines available VRAM 
+
+        Args:
+            model_stats (dict): relevant model stats (param count, layer count, etc)
+            total_vram (float): total available VRAM for current host machine
+        """ 
+        return 2048 # TODO: Remove me 
+
+
+    
+    def _get_model_stats(self):
+        """
+        Retrieve relevant model stats for Ollama model 
+
+        Information retrieved:
+            a) model_context_length: the maximum number of tokens LLM can process in single conversation 
+            b) parameter count: number of parameters utilzied in model
+            c) quantization_level: the level this models parameters are quantized (requried for determing how many bytes required per parameter)
+            d) num_layers: number of layers in the model
+            e) hidden_dimensions: size of internal representations at each layer (length of vectors)
+        """
+
+        try:
+            data = {
+                "model": self.model_name
+            }
+
+            response = requests.post(os.getenv("OLLAMA_BASE_URL", settings.OLLAMA_LOCAL_HOST_URL) + "/api/show", json=data)
+            response.raise_for_status()
+
+            response_data = response.json()
+            model_info = response_data["model_info"]
+
+            # extract relevant names that response fields are prefixed with 
+            model_architecture_name = model_info["general.architecture"]
+
+            return {
+                "model_context_length": model_info[f"{model_architecture_name}.context_length"],
+                "parameter_count": model_info[f"general.parameter_count"], 
+                "quantization_level": response_data['details']['quantization_level'], 
+                "num_layers": model_info['block_count'], 
+                "hidden_dimensions": model_info['embedding_length'] 
+            }
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Failure occurred while attempting to retrieve model stats", exc_info=True)
+            raise e
+
+
+
+    
+
+    def _get_total_vram(self): 
+        """
+        Get the total VRAM for the current machine's GPU 
+
+        Note: VRAM is essentially the working space for LLMs, everything must fit into
+        this space: a) the model weights, b) KV-cache, c) input/output token generation, etc
+
+        TODO: Consider making this an abstract method in LLMBase if needed (may only be required for Ollama due to local usage)
+        """
+
+        # ensure that GPU device is avaialble 
         if torch.cuda.is_available():
             device = torch.device("cuda")
             total_vram_bytes = torch.cuda.get_device_properties(device=device).total_memory
@@ -31,21 +101,7 @@ class OllamaLLM(LLMBase):
 
         else:
             logger.warning("CUDA is not available. Ollama LLM may have limited performance on CPU-only systems.")
-            # TODO: Consider setting default max length of CPU based systems 
-        
-        # get the maximum context length of configured model 
-        
-
-        """
-        TODO: 
-            1) Hit Ollama http://localhost:11434/api/tags endpoint to get model context length 
-                    - Detemrine if any other relevant information is needed 
-            2) Calculate available VRAM on host machine 
-                    - consider caching somewhere to avoid repeated calls (i.e db)
-            3) Restrict max context length accoridngly 
-        """
-
-        return 2048  # TODO: Implement me and remove place holder value 
+            # TODO: Consider setting default max length of CPU based systems or not allowing for usage 
     
     def tokenize(self, text: str) -> list[str]:
         """
@@ -69,7 +125,7 @@ class OllamaLLM(LLMBase):
 
         try:
             # check ollama server is running and get pulled model info
-            response = requests.get(os.getenv("OLLAMA_BASE_URL", "http://localhost:11434") + "/api/tags")
+            response = requests.get(os.getenv("OLLAMA_BASE_URL", settings.OLLAMA_LOCAL_HOST_URL) + "/api/tags")
             response.raise_for_status()
 
             # validate model is available 
