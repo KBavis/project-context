364d1c00a3ff3c9ea31ce4875d6012d4eda74667
Kellen Bavis <kellenrbavis@gmail.com>
2026-01-23 20:37:33 -0500

    feat: init stubbing of creating conversation logic

--- apps/backend/app/services/conversation.py
+++ apps/backend/app/services/conversation.py
@@ -1,25 +1,79 @@
 
-from app.pydantic import ChatRequest
+from app.pydantic import CreateConversationRequest, UpdateConversationRequest
 from app.models import Conversation
+from app.base import settings
+from app.llm import LLMManager
 
 from sqlalchemy.orm import Session
 
 from uuid import UUID
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 class ConversationService:
 
-    def __init__(self, db: Session):
+    def __init__(
+        self, 
+        db: Session,
+        llm_manager: LLMManager
+    ):
         self.db = db 
+        self.llm_manager = llm_manager
     
 
-    def create_conversation(self, chat: ChatRequest):
+    def create_conversation(self, conversation: CreateConversationRequest):
         """
-        Create a new conversation with a specified LLM 
+        Create a new conversation with the current configured LLM 
 
         Args:
-            chat (ChatRequest): content of user sent Message and specified Project it relates to 
+            conversation (CreateConversationRequest): content of user sent Message and specified Project it relates to 
         """
+
+        logger.info(f"Creating Conversation for project {conversation.project_id} with LLM {conversation.ll_model_name} and provider {conversation.ll_model_provider}")
+
+        # validate llm model name and provider if passed 
+        self._validate_llm_model(conversation.ll_model_name, conversation.ll_model_provider)
+
+
+
+        # self.db.add(Conversation(
+        #     project_id=conversation.project_id,
+        #     ll_model_name=conversation.ll_model_name,
+        #     ll_model_provider=conversation.ll_model_provider
+        # ))
+
+
+    def _validate_llm_model(self, model_name: str, model_provider: str):
+        """
+        Validate the specified LLM model name and provider OR return 
+        the currently configured LLM model name and provider
+
+        Args:
+            model_name (str): name of LLM model 
+            model_provider (str): provider of LLM model 
+        """
+        
+
+        if model_provider not in settings.VALID_LL_MODEL_PROVIDERS:
+            raise ValueError(f"Invalid LLM model provider: {model_provider}")
+
+        
+        # attempt to create LLM instance to validate model name 
+        try:
+            llm = self.llm_manager.get_llm()
+        except ValueError as e:
+            raise ValueError(f"Invalid LLM model name: {model_name}")
+
+    
+        
+
+
+
+
+    
+
     
 
 
@@ -32,10 +86,10 @@ class ConversationService:
         """
     
 
-    def update_conversation(self, chat: ChatRequest):
+    def update_conversation(self, conversation: UpdateConversationRequest):
         """
         Continue existing conversation with specified LLM
 
         Args:
-            chat (ChatRequest): content of user sent Message and specified Project it relates to 
+            conversation (UpdateConversationRequest): content of user sent Message and specified Project it relates to 
         """
