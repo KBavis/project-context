b1283af41b934702502ac7ea0fce0b2af27a473f
Kellen Bavis <kellenrbavis@gmail.com>
2026-02-09 21:16:31 -0500

    feat: kick off logic to create conversation summary if needed

--- apps/backend/app/services/message.py
+++ apps/backend/app/services/message.py
@@ -1,8 +1,8 @@
+from app.models import Conversation
 from app.pydantic import MessageRequest
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.services.conversation import ConversationService
-from app.services.query import QueryService
 
 from uuid import UUID
 
@@ -10,38 +10,61 @@ class MessageService:
     def __init__(
         self, 
         db: AsyncSession,
-        conversation_svc: ConversationService,
-        query_svc: QueryService
     ):
         self.db = db
-        self.conversation_svc = conversation_svc
-        self.query_svc = query_svc
 
-    def send_message(self, message: MessageRequest, conversation_id: UUID):
+        # initalize the Conversation Service 
+        self.conversation_svc = self.init_conversation_svc()
+
+
+    def init_conversation_svc(self):
+        """
+        Initialize the Conversation Service
+        """
+        return ConversationService(
+            db=self.db,
+            llm_manager=self.llm_manager
+        )
+
+    async def send_message(self, message: MessageRequest, conversation_id: UUID):
         """
         Functionality to send a message to a previously created Conversation
         """
 
         # retrieve conversation 
-        conversation = self.conversation_svc.get_conversation(conversation_id)
+        conversation = await self.conversation_svc.get_conversation(conversation_id)
         if not conversation:
             raise Exception(f"Conversation with id {conversation_id} not found")
 
-        
-        # gather existing context from previously sent messages 
-
         # add summary to conversation if this is the first sent message 
+        if conversation.summary is None:
+            await self.conversation_svc.create_conversation_summary(conversation, message.content)
+
+        # gather existing context from previously sent messages 
 
         # determine if this question requires new chunks to be retrieved (or if its a follow up question that can be answered using existing context)
 
-        # retrieve chunks via query service 
+        # retrieve relevant chunks via query service 
 
         # generate prompt for LLM leveraging query service 
 
         # send new prompt / chunks along with existing context to LLM 
 
         # stream response from LLM back to user 
+    
 
+    def get_previous_messages(self, conversation: Conversation):
+        """
+        Functionality to retrieve all previous messages for a specific Conversation
+
+        Args:
+            conversation (Conversation): Conversation to retrieve previous messages for
+        """ 
+
+        messages = conversation.messages
+
+        # TODO: Filter messages by user and LLM 
+        return None
 
         
 
