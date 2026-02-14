c675eae94ae3559c4ef5394f17f63103e2662e9f
Kellen Bavis <kellenrbavis@gmail.com>
2026-02-13 20:21:54 -0500

    feat: add functionality for determining a question type (if it needs context or not) and saving a message in msg svc

--- apps/backend/app/services/message.py
+++ apps/backend/app/services/message.py
@@ -50,11 +50,86 @@ class MessageService:
         # send new prompt / chunks along with existing context to LLM 
 
         # stream response from LLM back to user 
-    
 
-    def get_previous_messages(self, conversation: Conversation) -> dict[str, List]:
+        
+        
+
+    async def save_message(
+        self, 
+        message: str, 
+        sender: Sender, 
+        sequence_number: int,
+        content_type: str, # TODO: Create Enum for valid types 
+        token_count: int,
+        conversation_id: UUID
+    ):
         """
-        Functionality to retrieve all previous messages for a specific Conversation
+        Functionality to save a message to a previously created Conversation
+
+        Args:
+            message (str): Message to be saved
+            sender (Sender): Sender of the message
+            sequence_number (int): Sequence number of the message
+            conversation_id (UUID): ID of the conversation to save the message to
+        """
+
+        # create message in database 
+        msg_to_save = Message(
+            message=message,
+            sender=sender,
+            sequence_number=sequence_number,
+            content_type=content_type,
+            token_count=token_count,
+            conversation_id=conversation_id
+        )
+
+        self.db.add(msg_to_save)
+        await self.db.flush()
+
+        return msg_to_save
+
+
+    async def determine_question_type(self, prompt: str, formatted_messages: str, llm_manager: LLMManager) -> QuestionType: 
+        """
+        Determine if the current question requires new chunks to be retrieved (or if its a follow up question that can be answered using existing context)
+
+        Args:
+            prompt (str): The user's question
+            formatted_messages (str): Formatted messages from the conversation, seperated by sender
+            llm_manager (LLMManager): LLM Manager instance to be used for the query
+        """
+        
+        determine_question_type_prompt = f"""
+        You are a helpful assistant that determine if a user's question requires some additional context to be answered, 
+        or if it can simply be answered using the existing conversation history. 
+
+        The messages will be formatted as follows: "sender:<message>" and will be ordered by oldest to latest. 
+
+        If you believe that the answer could be answered using the existing conversation history, return "follow_up" as the response. 
+        If you believe that the answer requires new chunks to be retrieved, return "new_chunks" as the response.
+
+        User Question: {prompt}
+        Existing Messages: {formatted_messages}
+        """
+
+        llm = llm_manager.get_llm()
+
+        try:
+            logger.debug(f"Determining QuestionType for the Message={prompt}")
+            response = await llm.send_message(determine_question_type_prompt)
+        except Exception as e:
+            logger.error(f"Error determining question type: {e}")
+            return QuestionType.UNKNOWN
+
+        return QuestionType(response.text) if response and response.text else QuestionType.UNKNOWN
+        
+
+
+    def get_previous_k_messages(self, conversation: Conversation, k: int = 10) -> str:
+        """
+        Functionality to retrieve the last k messages for a specific Conversation
+
+        TODO: Account for differnet content types 
 
         Args:
             conversation (Conversation): Conversation to retrieve previous messages for
