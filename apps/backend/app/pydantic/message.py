dddaaad43cd4a51506fd09223377ae6cb6c0ad95
Kellen Bavis <kellenrbavis@gmail.com>
2026-02-14 19:52:11 -0500

    feat: add pydantic objs for response

--- apps/backend/app/pydantic/message.py
+++ apps/backend/app/pydantic/message.py
@@ -1,8 +1,27 @@
 
+from datetime import datetime
 from pydantic import BaseModel
+from uuid import UUID
 
 
 class MessageRequest(BaseModel):
     content: str
     content_type: str = "text"
+
+
+class MessageDto(BaseModel):
+    content: str
+    content_type: str
+    token_count: int
+    sequence_number: int
+    created_at: datetime
+    updated_at: datetime
+
+
+class MessageResponse(BaseModel): 
+    
+    user_message: MessageDto 
+    model_message: MessageDto 
+    conversation_id: UUID
+
     
