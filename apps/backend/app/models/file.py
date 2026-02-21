2eeca2047c9e0cff2385660b43c4146ad506cd64
Kellen Bavis <kellenrbavis@gmail.com>
2026-02-21 18:49:02 -0500

    feat: back reference to the citation associated with this file

--- apps/backend/app/models/file.py
+++ apps/backend/app/models/file.py
@@ -71,3 +71,8 @@ class File(Base):
     file_collections: Mapped[List["FileCollection"]] = relationship(
         back_populates="file", cascade="all, delete-orphan"
     )
+
+    # one to one relationship with Citation
+    citation: Mapped["Citation"] = relationship(
+        back_populates="file"
+    )   
