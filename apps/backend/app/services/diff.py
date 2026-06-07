637da2361301d7e222a9b32fa40d7af75e21af97
Kellen Bavis <kellenrbavis@gmail.com>
2026-06-07 16:55:36 -0400

    feat: add logic to dynamically determine the latest commit we persisted for a repository/rpoject and to leverage new enhanced state of the commits retrieved

--- apps/backend/app/services/diff.py
+++ apps/backend/app/services/diff.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import logging
 from uuid import UUID 
+from datetime import datetime, timezone
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
@@ -9,12 +12,14 @@ from app.services.project import ProjectService
 from app.models.data_source import DataSourceType, DataSource
 from app.models.project import Project
 from app.models.project_repository_changes import ProjectRepositoryChanges
-from app.models.file_diff import FileDiff
+from app.models.file_diff import FileDiff, ChangeType
+from app.models.git_commit import GitCommit
 from app.data_providers.fetchable.issue_tracker import IssueTrackerDataProvider
 from app.data_providers.ingestible.repository import RepositoryDataProvider
-
+from app.pydantic.git_commit import GitCommitDetail
 
 logger = logging.getLogger(__name__)
+
 
 class DiffService:
     """
@@ -44,20 +49,18 @@ class DiffService:
         Sync the updates made to a `Repository DataSource`
             - Optionally specify a Project ID to limit the scope of the sync to a particular Project
         
-
         TODO: Handle failures gracefully (this should likely be a Transactional process, sort of hard to do with Chroma & git though)
-
+        
         Args:
-            data_source_id (UUID): The ID of the Data Source to sync.
+            repository_data_source_id (UUID): The ID of the Data Source to sync.
+            ingestion_job_id (UUID): The ID of the IngestionJob updating this record.
             project_ids (list[UUID] | None): The IDs of the Projects to limit the sync to.
         """
-
         # validate that the provided DataSource is of type `REPOSITORY` and configured properly for syncing 
         repository_ds = await self.data_source_svc.aget_data_source_by_id(data_source_id=repository_data_source_id)
         if repository_ds.type != DataSourceType.REPOSITORY or not repository_ds.scope_by_issues:
             logger.warning(f"DataSource with ID {repository_data_source_id} is not configured for repository syncing. Skipping sync.")
             return
-
 
         # if no projects are provided, sync the repository for ALL projects 
         if not project_ids:
@@ -81,13 +84,11 @@ class DiffService:
                 logger.error(f"Project with ID {project_id} does not have any Parent Issues configured. This is invalid as any configured Project for DataSource of type REPOSITORY and scope_by_issues=True requires this information")
                 raise Exception(f"Project with ID {project_id} does not have any Parent Issues configured, which is required for Repository syncing")
 
-
             # validate IssueTracker DataSource configured for this Project (if != 1 for Project, this will error out)
             issue_tracker_ds = await self.data_source_svc.get_issue_tracker_data_source(project_id)
 
-            # determine if there are any existing ProjectRepositoryChanges for this Project/DataSource 
-            project_repository_changes = await self.get_project_repository_changes(project_id=project_id, data_source_id=repository_data_source_id)
-            commit_hashes = await self.get_new_repository_commits(
+            # fetch new commits details
+            new_commits = await self.get_new_repository_commits(
                 issue_tracker_ds,
                 repository_ds,
                 project,
@@ -107,50 +108,53 @@ class DiffService:
             self, 
             issue_tracker_ds: DataSource, 
             repository_ds: DataSource,
-            project: Project, 
-            project_repository_changes: ProjectRepositoryChanges | None = None
-    ):
+            project: Project
+    ) -> list[GitCommitDetail]:
         """
-        Process new repository commits that have been added as a result of specified Project 
-            - get the lastest `child_issues` configured for Project
-            - determine if there are any new commits that need to be processed
-            - if so, return back newly found commit hashes 
+        Process new repository commits that have been added as a result of specified Project.
         """
 
-        # extract prior commit hash from last time syncing Repository & Project changes 
-        prior_commit_hash = project_repository_changes.last_seen_commit if project_repository_changes else None
+        # determine the latest commit datetime dynamically
+        stmt = (
+            select(GitCommit)
+            .where(GitCommit.project_id == project.id, GitCommit.data_source_id == repository_ds.id)
+            .order_by(GitCommit.commit_datetime.desc())
+            .limit(1)
+        )
+        res = await self.async_db.execute(stmt)
+        latest_commit = res.scalar_one_or_none()
+        last_sync_time = latest_commit.commit_datetime if latest_commit else None
 
         # get the child issues from Project.parent_issues 
         issue_data_provider = IssueTrackerDataProvider.from_provider(issue_tracker_ds)
         child_issues = await issue_data_provider.get_issues(project.parent_issues)
         if not child_issues:
             logger.warning("No child issues found -- skipping processing repository changes")
-            return None
+            return []
 
         repository_data_provider = RepositoryDataProvider.from_provider(repository_ds)
         
-        # determine if latest state of ProjectRepositoryChanges is up-to-date with state in Repository (skip if first time sync)
-        if prior_commit_hash:
-            logger.info(f"Existing ProjectRepositoryChanges found for Project={project.id} and DataSource={repository_ds.id}. Checking for new commits since last sync.")
+        # determine if latest state is up-to-date with state in Repository (skip if first time sync)
+        if latest_commit:
+            logger.info(f"Existing GitCommits found for Project={project.id} and DataSource={repository_ds.id}. Checking for new commits since last sync.")
             latest_repository_hash = await repository_data_provider.get_latest_commit_sha(child_issues)
             if not latest_repository_hash:
-                logger.info(f"No commits found for Repository={repository_ds.id}: skipping syncing")
-                return None
+                raise Exception(
+                    f"Inconsistent state detected: Local DB has active commits for Project {project.id}, "
+                    f"but remote repository search returned no matching commits. This may be caused by a "
+                    f"change in configured parent issues or a remote history change."
+                )
 
-            # determine if the repository is synced or not based on commit retreived
-            if prior_commit_hash == latest_repository_hash:
+            # determine if the repository is synced or not based on commit retrieved
+            if latest_commit.commit_hash == latest_repository_hash:
                 logger.info(f"The previously sycned state of Repository with DataSource={repository_ds.id} and Project={project.id} is up-to-date -- skipping re-syncing")
-                return None
+                return []
 
-        # NOTE: At this point, we've determined new commits have been made regarding this Project/DataSource -- resyncing is necessary
+        # At this point, new commits have been made regarding this Project/DataSource -- resyncing is necessary
         logger.warning(f"Repository DataSource={repository_ds.id} code changes from Project={project.id} aren't synced: accounting for new commits")
-
-        last_sync_time = project_repository_changes.last_synced_time if project_repository_changes else None
-        commit_hashes = await repository_data_provider.get_all_commit_sha(child_issues, last_sync_time)
-
-        logger.info(f"Found {len(commit_hashes)} for Project={project.id} and DataSource={repository_ds.id}")
-        return commit_hashes
-
+        new_commits = await repository_data_provider.get_all_commits_info(child_issues, last_sync_time)
+        logger.info(f"Found {len(new_commits)} new commits for Project={project.id} and DataSource={repository_ds.id}")
+        return new_commits
 
 
     async def get_total_repository_code_changes(self, project_id: UUID, data_source_id: UUID | None = None) -> list[dict]:
