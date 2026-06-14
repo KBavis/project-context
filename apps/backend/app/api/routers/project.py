0da9c671c1d89169174dd0aec531fea98a954948
Kellen Bavis <kellenrbavis@gmail.com>
2026-06-14 15:20:46 -0400

    feat: when linking a data source to a Project, this will imply that the diff sync job should be fired off in the case that the data source is a REPOSITORY and has scope by changes set to True

--- apps/backend/app/api/routers/project.py
+++ apps/backend/app/api/routers/project.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, BackgroundTasks
 
 from app.services import ProjectService
 from app.pydantic import ProjectRequest
-from ..svc_deps import get_project_svc
+from app.models.data_source import DataSourceType
+from ..svc_deps import get_project_svc, get_async_diff_svc, get_data_source_svc
+from app.services.diff import DiffService
+from app.services.data_source import DataSourceService
 
 from typing import List
 from uuid import UUID
@@ -46,17 +49,29 @@ def get_projects(
 
 
 @router.post("/{project_id}/data-sources/{data_source_id}", summary="Link data source to project")
-def link_data_source(
+async def link_data_source(
     project_id: UUID,
     data_source_id: UUID,
-    svc: ProjectService = Depends(get_project_svc)
+    background_tasks: BackgroundTasks,
+    svc: ProjectService = Depends(get_project_svc),
+    diff_svc: DiffService = Depends(get_async_diff_svc),
+    ds_svc: DataSourceService = Depends(get_data_source_svc)
 ):
     """
     Associate an existing Data Source with a Project
     """
 
     try:
-        return svc.link_data_source_to_project(project_id, data_source_id)
+        res = svc.link_data_source_to_project(project_id, data_source_id)
+        
+        # kick of DiffSyncJob for RepositoryDataSource if its scoped_by_issues when first linking Data Source & Project 
+        # this runs in the background and the consumer of this endpoint will not need to wait for this to finish processing
+        ds = await ds_svc.aget_data_source_by_id(data_source_id)
+        if ds.type == DataSourceType.REPOSITORY and ds.scope_by_issues:
+            job = await diff_svc.init_diff_sync_job(project_id, data_source_id)
+            background_tasks.add_task(diff_svc.execute_repository_sync_job, job.id)
+            
+        return res
     except ValueError as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     except Exception as e:
